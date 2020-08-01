51e0e77008e52849f79e384e10818731e54ee095
Deeshan Sharma <princedeeshansharma@gmail.com>
2020-08-01 17:27:43 +0530

    Added a feature to see the details of the user

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+'''
+Author: Deeshan Sharma
+Date: July 7, 2020
+Purpose: Copy of Banking System with all of the main available process with the record keeping Database of the users.
+'''
+
 import sqlite3 as sq
 from time import sleep
 import datetime as dt
@@ -11,7 +17,8 @@ class Bank():
         print("2. Deposit")
         print("3. Balance Status") 
         print("4. Open New Account")
-        print("5. Exit")
+        print("5. See Details")
+        print("6. Exit")
         while True:
             ch = int(input("Enter your choice = "))
             if ch == 1:
@@ -21,8 +28,10 @@ class Bank():
             elif ch == 3:
                 self.balStatus()
             elif ch == 4:
-                self.newAcc()            
+                self.newAcc()
             elif ch == 5:
+                self.seeDetails()
+            elif ch == 6:
                 print("Thanks for using our service. Do come again.")
                 self.cur.close()
                 self.conn.close()
@@ -164,6 +173,41 @@ class Bank():
         accno = self.cur.fetchall()
         print("Conguragtulations...!! Your account is successfully created.")
         print(f"Your account number is {accno[0][0]}. Please remember this as it is very important you will require it while doing any transaction.")
+
+    def seeDetails(self,):
+        accNo = int(input("Enter your Account no = "))
+        query = ("select Account_no from Accounts where Account_No > 0")
+        self.cur.execute(query)
+        temp = self.cur.fetchall()
+        data_accNo = [i[0] for i in temp]
+        counter = 0
+        trails = 2
+        if accNo in data_accNo:
+            pin = int(input("Enter your pin no = "))
+            query = (f"select Pin from Accounts where Account_no = {accNo}")
+            self.cur.execute(query)
+            data_pin = self.cur.fetchall()[0][0]
+            while counter != 3:
+                if data_pin == pin:
+                    query = (f"select * from Accounts where Account_no = {accNo}")
+                    self.cur.execute(query)
+                    accDetails = self.cur.fetchall()[0]
+                    print(f"Your Details are as Follows\n{accDetails}")
+                    print("Thanks for using our service")
+                    break
+                else:
+                    print("Incorrect Pin. Please Try again.")
+                    if trails > 0:
+                        print(f"You got {trails} more incorrect trials left.")
+                    trails -= 1
+                    counter += 1
+                    if counter == 3:
+                        print("Incorrect pin trials over please start over.")
+                        break
+                    pin = int(input("Enter your pin no = "))
+                    continue
+        else:
+            print("No Record Found in Our Database. Please Try Again...!")
 
 if __name__ == "__main__":
     a = Bank()
